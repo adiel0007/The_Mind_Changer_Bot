6526a434b232e7863deff76c7d9ec44e784cda19
adiel0007 <adiel.007@hotmail.com>
2026-06-26 07:37:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,7 @@ def calculate_rsi(series, period=14):
     gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
     rs = gain / loss
-    return 100 - (100 / (1fr + rs))
-
+return 100 - (100 / (1 + rs))
 def scan_market(ticker_list, mode):
     results = []
     
